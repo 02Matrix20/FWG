e854123085aa002ca0876e37b9ba11bfb7f3830d
02Matrix20 <65680882+02Matrix20@users.noreply.github.com>
2022-10-17 01:09:02 +0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,12 @@
+let menuBtn = document.querySelector('.menu_btn');
+let menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+	document.body.classList.toggle('lock');
+  body
+ 
+})
+
+
